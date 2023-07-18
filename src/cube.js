2659f8f789c0589bfa7cd76ad90c2324aed9654d
Connor McElroy <connor.mcelroy@outlook.com>
2023-07-18 14:21:16 -0500

    cleanup

--- src/cube.js
+++ src/cube.js
@@ -2,7 +2,6 @@ import cubejs from '@cubejs-client/core';
 import * as api from './api';
 
 const CUBEJS_API_URL = 'https://peaceful-condor.aws-us-east-1.cubecloudapp.dev/cubejs-api/v1';
-//const CUBEJS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODQ4Njg1MjV9.OOpFYQpPirzs7UffePGgJAFsfxnAKp-CWgPva785AEo';
 
 let apiTokenPromise;
 
