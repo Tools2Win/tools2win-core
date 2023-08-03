0be1a24758bf762fb8633d65b9b05e380dd6e542
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-03 13:24:01 -0500

    split entry

---
+++ src/web.js
@@ -0,0 +1,4 @@
+export { AuthenticationManager as ReactAuthenticationManager } from './reactjs/components';
+export { default as useAuthContext } from './hooks/useAuthContext';
+export { default as useApi } from './hooks/useApi';
+export { default as useCubeData } from './hooks/useCubeData';
