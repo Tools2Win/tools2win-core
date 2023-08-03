0980b9fbca2b9125275a0fd880659b73eb658b19
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-03 13:54:15 -0500

    firebase persistence fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, browserLocalPersistence } from "firebase/auth";
+import { initializeAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBttlwwYOOVFfTw1yioOjWSZazf5dZNFl0",
     authDomain: "tools2win.firebaseapp.com",
@@ -13,23 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// Check if the environment is React Native
-const isReactNative = () => {
-    return (
-        typeof navigator === 'object' &&
-        navigator.product === 'ReactNative'
-    );
-}
-
-// Conditionally set persistence based on environment
-let persistence;
-
-if (isReactNative()) {
-    const AsyncStorage = require("@react-native-async-storage/async-storage").default;
-    const { getReactNativePersistence } = require('firebase/auth/react-native');
-    persistence = getReactNativePersistence(AsyncStorage);
-} else {
-    persistence = browserLocalPersistence;
-}
-
-export const auth = initializeAuth(app, { persistence });
+export const auth = initializeAuth(app);
