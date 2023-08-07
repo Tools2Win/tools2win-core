6978ed9144c3e01c58682bdce6e9ea2c88155ba8
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-07 12:14:29 -0500

    test

--- src/web.js
+++ src/web.js
@@ -1,4 +1,4 @@
-export { default as AuthenticationManager } from './reactjs/components';
 export { default as useAuthContext } from './hooks/useAuthContext';
 export { default as useApi } from './hooks/useApi';
 export { default as useCubeData } from './hooks/useCubeData';
+export { default as AuthenticationManager } from './reactjs/components/AuthenticationManager';
