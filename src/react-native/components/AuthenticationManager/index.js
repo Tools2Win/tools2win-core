735cd777abad1786af8bc72562cfc0db601c3f13
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-24 09:37:42 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -1,16 +1,16 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import useAuth from '../../../hooks/useAuth';
-import { NavigationContainer } from '@react-navigation/native';
-import Acknowledgment from '../../screens/Acknowledgment';
-import NoDisplayName from '../../screens/NoDisplayName';
-import NoClient from '../../screens/NoClient';
-import SalesmanSelection from '../../screens/SalesmanSelection';
-import SignIn from '../../screens/SignIn';
-import ForgotPassword from '../../screens/ForgotPassword';
-import SignUp from '../../screens/SignUp';
-import AuthContext from '../../../contexts/AuthContext';
+// import useAuth from '../../../hooks/useAuth';
+// import { NavigationContainer } from '@react-navigation/native';
+// import Acknowledgment from '../../screens/Acknowledgment';
+// import NoDisplayName from '../../screens/NoDisplayName';
+// import NoClient from '../../screens/NoClient';
+// import SalesmanSelection from '../../screens/SalesmanSelection';
+// import SignIn from '../../screens/SignIn';
+// import ForgotPassword from '../../screens/ForgotPassword';
+// import SignUp from '../../screens/SignUp';
+// import AuthContext from '../../../contexts/AuthContext';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 const AuthenticationManager = ({ children }) => {
     //const { loading, user, signOut } = useAuth();
