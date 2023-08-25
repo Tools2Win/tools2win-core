d670d046979d4f6907780750ff05f1a763810a42
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-25 11:45:10 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -1,11 +1,11 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import useAuth from '../../../hooks/useAuth';
 import { Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
 //import Acknowledgment from '../../screens/Acknowledgment';
 //import NoDisplayName from '../../screens/NoDisplayName';
 //import NoClient from '../../screens/NoClient';
 //import SalesmanSelection from '../../screens/SalesmanSelection';
-import SignIn from '../../screens/SignIn';
 // import ForgotPassword from '../../screens/ForgotPassword';
 // import SignUp from '../../screens/SignUp';
 //import AuthContext from '../../../contexts/AuthContext';
@@ -18,9 +18,7 @@ const AuthenticationManager = ({ children }) => {
     if (loading) return <Text>asdfjkl;</Text>;
 
     if (!user) {
-        return (
-            <Text>yehhh</Text>
-        );
+        return <Text>getting along</Text>>
     }
     return children;
 
