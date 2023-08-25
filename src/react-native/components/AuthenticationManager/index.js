a364808bc2c59d02d805ac03f8b2e65d2e744a72
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-25 09:21:28 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -6,7 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 //import NoDisplayName from '../../screens/NoDisplayName';
 //import NoClient from '../../screens/NoClient';
 //import SalesmanSelection from '../../screens/SalesmanSelection';
-import SignIn from '../../screens/SignIn';
+//import SignIn from '../../screens/SignIn';
 // import ForgotPassword from '../../screens/ForgotPassword';
 // import SignUp from '../../screens/SignUp';
 //import AuthContext from '../../../contexts/AuthContext';
@@ -18,19 +18,21 @@ const AuthenticationManager = ({ children }) => {
 
     if (loading) return <Text>asdfjkl;</Text>;
 
+    return <Text>WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYY</Text>
 
-    if (!user) {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Signin" component={SignIn} options={{ headerShown: false }} />
-                    {/* <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} /> */}
-                    {/* <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Sign Up' }} /> */}
-                </Stack.Navigator>
-            </NavigationContainer>
-        );
-    }
-    return children;
+
+    // if (!user) {
+    //     return (
+    //         <NavigationContainer>
+    //             <Stack.Navigator>
+    //                 <Stack.Screen name="Signin" component={SignIn} options={{ headerShown: false }} />
+    //                 {/* <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} /> */}
+    //                 {/* <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Sign Up' }} /> */}
+    //             </Stack.Navigator>
+    //         </NavigationContainer>
+    //     );
+    // }
+    // return children;
 
 
 
