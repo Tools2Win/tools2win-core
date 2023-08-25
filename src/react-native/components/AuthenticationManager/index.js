41b6f054533c9c0423c3cd285b9aac8f8b734d12
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-25 11:58:59 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import useAuth from '../../../hooks/useAuth';
 import { Text } from 'react-native'
