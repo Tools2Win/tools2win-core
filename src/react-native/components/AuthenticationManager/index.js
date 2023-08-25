58d1de4dd96d5fd792eafa828455ffa729bda2df
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-25 08:58:02 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -1,12 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import useAuth from '../../../hooks/useAuth';
 import { Text } from 'react-native'
-//import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 //import Acknowledgment from '../../screens/Acknowledgment';
 //import NoDisplayName from '../../screens/NoDisplayName';
 //import NoClient from '../../screens/NoClient';
 //import SalesmanSelection from '../../screens/SalesmanSelection';
-// import SignIn from '../../screens/SignIn';
+import SignIn from '../../screens/SignIn';
 // import ForgotPassword from '../../screens/ForgotPassword';
 // import SignUp from '../../screens/SignUp';
 //import AuthContext from '../../../contexts/AuthContext';
