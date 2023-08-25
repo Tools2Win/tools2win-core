715acc7107604411eb7be6ae0720090f9c361444
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-25 16:05:08 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -1,7 +1,15 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Test from '../../components/Test';
+import useAuth from '../../../hooks/useAuth';
+import { NavigationContainer } from '@react-navigation/native';
+import Acknowledgment from '../../screens/Acknowledgment';
+import NoDisplayName from '../../screens/NoDisplayName';
+import NoClient from '../../screens/NoClient';
+import SalesmanSelection from '../../screens/SalesmanSelection';
+import SignIn from '../../screens/SignIn';
+import ForgotPassword from '../../screens/ForgotPassword';
+import SignUp from '../../screens/SignUp';
+import AuthContext from '../../../contexts/AuthContext';
+
 
 const Stack = createStackNavigator();
 
