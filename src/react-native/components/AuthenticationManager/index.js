e900f32021975a6c44b7de3645fc6a9599812a8c
Connor McElroy <connor.mcelroy@outlook.com>
2023-08-25 15:06:07 -0500

    test

--- src/react-native/components/AuthenticationManager/index.js
+++ src/react-native/components/AuthenticationManager/index.js
@@ -1,44 +1,18 @@
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import useAuth from '../../../hooks/useAuth';
-import { Text } from 'react-native'
-import Test from '../../components/Test'
-//import Acknowledgment from '../../screens/Acknowledgment';
-//import NoDisplayName from '../../screens/NoDisplayName';
-//import NoClient from '../../screens/NoClient';
-//import SalesmanSelection from '../../screens/SalesmanSelection';
-// import ForgotPassword from '../../screens/ForgotPassword';
-// import SignUp from '../../screens/SignUp';
-//import AuthContext from '../../../contexts/AuthContext';
+import Test from '../../components/Test';
 
 const Stack = createStackNavigator();
 
-const AuthenticationManager = ({ children }) => {
-    const { loading, user, signOut } = useAuth();
-
-    if (loading) return <Text>asdfjkl;</Text>;
-
-    if (!user) {
-        return (
+const AuthenticationManager = () => {
+    return (
+        <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Test" component={Test} options={{ headerShown: false }} />
-                {/* <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} /> */}
-                {/* <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Sign Up' }} /> */}
             </Stack.Navigator>
-        );
-    }
-    return <Text>Interest</Text>;
-
-    // if (!user.emailVerified) return <Acknowledgment />;
-    // if (!user.displayName) return <NoDisplayName />;
-    // if (!user.clientID) return <NoClient />;
-    // if (!user.salesmanCode) return <SalesmanSelection />;
-
-    // return (
-    //     <AuthContext.Provider value={{ user, signOut }}>
-    //         {children}
-    //     </AuthContext.Provider>
-    // );
+        </NavigationContainer>
+    );
 };
 
 export default AuthenticationManager;
