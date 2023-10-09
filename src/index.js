2b17cd2daab07a4550dca5d3cdc7c609bcbce8c8
Connor McElroy <connor.mcelroy@outlook.com>
2023-10-09 12:21:34 -0500

    rne added

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// export { AuthenticationManager as ReactNativeAuthenticationManager, CubeProvider } from './react-native/components';
-// export { AuthenticationManager as ReactAuthenticationManager } from './reactjs/components';
-// export { default as useAuthContext } from './hooks/useAuthContext';
-// export { default as useApi } from './hooks/useApi';
-// export { default as useCubeData } from './hooks/useCubeData';
---
+++ src/themes/colors.js
@@ -0,0 +1,26 @@
+const colors = {
+    primary: {
+        main: '#F25929',
+        light: '#F89E85', // Example lighter shade
+        dark: '#B23C21', // Example darker shade
+    },
+    secondary: {
+        main: '#B2D136',
+        light: '#D4E572', // Example lighter shade
+        dark: '#87A024', // Example darker shade
+    },
+    background: {
+        default: '#F5F5F5',
+        paper: '#FFFFFF',
+    },
+    text: {
+        primary: '#333333',
+        secondary: '#7D7D7D',
+    },
+    additional: {
+        blue: '#A5C8E1',
+        purple: '#A89FBA',
+    },
+};
+
+export default colors;
