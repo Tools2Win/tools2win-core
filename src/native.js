a691a22cb325f38a67ca02e7ab4260408ae142c6
Connor McElroy <connor.mcelroy@outlook.com>
2023-10-10 10:41:35 -0500

    fix

--- src/native.js
+++ src/native.js
@@ -1,4 +1,5 @@
 export { default as useCubeData } from './hooks/useCubeData';
 export { default as AuthenticationManager } from './react-native/AuthenticationManager';
+export { default as CubeProvider } from './react-native/CubeProvider';
 export { default as useAuthContext } from './hooks/useAuthContext';
 export { default as useApi } from './hooks/useApi';
