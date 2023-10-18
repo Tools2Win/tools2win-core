b068ea95c4c67e590fb2d88a53d8ebbb8a34b325
Connor McElroy <connor.mcelroy@outlook.com>
2023-10-18 15:51:34 -0500

    colors added to webjs exports

--- src/web.js
+++ src/web.js
@@ -3,3 +3,4 @@ export { CubeProvider } from './reactjs/components';
 export { default as useAuthContext } from './hooks/useAuthContext';
 export { default as useApi } from './hooks/useApi';
 export { default as useCubeData } from './hooks/useCubeData';
+export { default as colors } from './themes/colors';
