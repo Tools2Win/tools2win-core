bb429856f5b310712fab706c1597add2db468ab4
Connor McElroy <connor.mcelroy@outlook.com>
2024-08-06 14:31:49 -0500

    added warning around preaggs not being used

--- src/hooks/useCubeData.js
+++ src/hooks/useCubeData.js
@@ -1,7 +1,12 @@
 import { useCubeQuery } from '@cubejs-client/react';
 
 const useCubeData = (query) => {
-    return useCubeQuery(query);
+    const { resultSet, error, isLoading } = useCubeQuery(query);
+    if (resultSet && resultSet.loadResponses[0].external !== true) {
+        console.warn('Preaggregation not used for query:', resultSet.loadResponses[0].query);
+    }
+
+    return { resultSet, error, isLoading };
 };
 
 export default useCubeData;
